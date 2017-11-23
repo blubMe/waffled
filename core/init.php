362daf5b171cff39ae78e8cc321f2f22ac79f503
Harpi <justharpi@gmail.com>
2017-11-23 11:34:12 +0700

    init file

---
+++ core/init.php
@@ -0,0 +1,9 @@
+<?php
+
+    // SESSION START
+    session_start();
+
+    // CALL FUNCTIONS
+    require_once('functions/conn.php');
+    require_once('functions/user.php');
+    require_once('functions/auth.php');
