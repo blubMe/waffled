3cb31f242b54a167e6418db460b45eea2e450dbb
Harpi <justharpi@gmail.com>
2017-11-24 12:55:14 +0700

    add partial file

--- edit-profile.php
+++ edit-profile.php
@@ -7,16 +7,7 @@ if ( !getSession('user') ) {
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Waffled</title>
-    <link rel="stylesheet" href="lib/css/style.css">
-    <script src="lib/js/jquery-3.2.1.min.js"></script>
-</head>
+require_once('templates/partial/');
 <body>
         <header class="homePageHeader">
                 <nav class="homePageNavbar">
---
+++ templates/partial/head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Waffled</title>
+    <link rel="stylesheet" href="lib/css/style.css">
+    <script src="lib/js/jquery-3.2.1.min.js"></script>
+</head>
